1b1ada1deb52a7fb031b3e4e02eb785c92ba788d
Iamlovingit <bitfrog@163.com>
2022-12-21 21:43:03 +0800

    update
    Signed-off-by: Iamlovingit <bitfrog@163.com>

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -14,8 +14,6 @@ function submitForm() {
         data.loginResult = result
         if (data.loginResult == 0) {
             router.push("/main")
-        } else {
-            console.log("result = 1")
         }
     });
   
@@ -24,28 +22,27 @@ function submitForm() {
 
 <template>
 <div class="login">
-    <el-card>
-        <el-alert title="用户名或密码错误" type="error" v-if="data.loginResult==1" />
-        <h2>欢迎登陆</h2>
-        <el-form :model="data" label-width="80px">
-        <el-form-item label="用户名称">
-            <el-input v-model="data.name" required="required"></el-input>
-        </el-form-item>
-        <el-form-item label="密码">
-            <el-input v-model="data.passwd" type="password" required="required"></el-input>
-        </el-form-item>
-        <el-form-item >
-            <el-button type="primary" @click="submitForm()">确定</el-button>
-            <el-button @click="cancel">取消</el-button>
-        </el-form-item>
-        </el-form>
-    </el-card>
+    <el-container>
+        <el-main>
+            <el-alert title="用户名或密码错误" type="error" v-if="data.loginResult==1" />
+            <h2>欢迎登陆</h2>
+            <el-form :model="data" label-width="80px">
+                <el-form-item label="用户名称">
+                    <el-input v-model="data.name" required="required"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input v-model="data.passwd" type="password" required="required"></el-input>
+                </el-form-item>
+                <el-form-item >
+                    <el-button type="primary" @click="submitForm()">确定</el-button>
+                    <el-button @click="cancel">取消</el-button>
+                </el-form-item>
+            </el-form>
+        </el-main>
+    </el-container>
 </div>
 </template>
 
 <style>
-.el-card {
-    height: 100%;
-}
 </style>
 
--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,3 +1,8 @@
 <template>
-    
+    <div>
+        <el-container>
+        <el-aside width="100px">Aside</el-aside>
+        <el-main>Main</el-main>
+    </el-container>
+    </div>
 </template>
