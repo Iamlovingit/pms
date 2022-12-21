aec37100dae53307a2855fcee4f25d8ac048283b
Iamlovingit <bitfrog@163.com>
2022-12-21 19:01:07 +0800

    update
    Signed-off-by: Iamlovingit <bitfrog@163.com>

--- app.go
+++ app.go
@@ -5,30 +5,6 @@ import (
 	"fmt"
 )
 
-type Records []Record
-
-type Record struct {
-	Name       string  `json:"name" db:"name"`
-	Department string  `json:"dep" db:"dep"`
-	ID         string  `json:"id" db:"di"`
-	BankID     string  `json:"bankid" db:"banid"`
-	January    float32 `json:"january" db:"januray"`
-	February   float32 `json:"february" db:"february"`
-	March      float32 `json:"march" db:"march"`
-	April      float32 `json:"april" db:"april"`
-	May        float32 `json:"may" db:"may"`
-	June       float32 `json:"june" db:"june"`
-	July       float32 `json:"july" db:"july"`
-	August     float32 `json:"august" db:"august"`
-	September  float32 `json:"september" db:"september"`
-	October    float32 `json:"october" db:"october"`
-	November   float32 `json:"november" db:"november"`
-	December   float32 `json:"december" db:"december"`
-	Average    float32 `json:"average" db:"average"`
-	Total      float32 `json:"total" db:"total"`
-	Bonus      float32 `json:"bonus" db:"bonus"`
-}
-
 // App struct
 type App struct {
 	ctx context.Context
@@ -50,12 +26,8 @@ func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
 }
 
-//
-func (a *App) GetRecords() Records {
-	var rs Records
-	r := Record{
-		Name: "cqs",
-	}
-	rs = append(rs, r)
-	return rs
+// SubmitLogin .
+func (a *App) SubmitLogin(name string, passwd string) int {
+	fmt.Print("call SubmitLogin")
+	return 0
 }
