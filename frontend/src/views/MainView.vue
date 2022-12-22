2f111c15f2aec2efb9a38dac51e57d354fabf688
Iamlovingit <bitfrog@163.com>
2022-12-22 18:32:42 +0800

    update
    Signed-off-by: Iamlovingit <bitfrog@163.com>

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,7 +1,26 @@
 <template>
     <div>
         <el-container>
-        <el-aside width="100px">Aside</el-aside>
+        <el-aside width="200px">
+            <el-menu
+            default-active="1">
+                <el-menu-item index="1">
+                    <template #title>
+                        人员维护
+                    </template>
+                </el-menu-item>
+                <el-menu-item index="2">
+                    <template #title>
+                        公积金录入
+                    </template>
+                </el-menu-item>
+                <el-menu-item index="3">
+                    <template #title>
+                        设置
+                    </template>
+                </el-menu-item>
+            </el-menu>
+        </el-aside>
         <el-main>Main</el-main>
     </el-container>
     </div>
