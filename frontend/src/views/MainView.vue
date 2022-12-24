b0a6a9318b7e0ad9f717163f3dfa6e7b5f23c6b7
Iamlovingit <bitfrog@163.com>
2022-12-24 18:17:34 +0800

    update
    Signed-off-by: Iamlovingit <bitfrog@163.com>

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,27 +1,65 @@
 <template>
     <div>
-        <el-container>
-        <el-aside width="200px">
-            <el-menu
-            default-active="1">
-                <el-menu-item index="1">
-                    <template #title>
-                        人员维护
-                    </template>
-                </el-menu-item>
-                <el-menu-item index="2">
-                    <template #title>
-                        公积金录入
-                    </template>
-                </el-menu-item>
-                <el-menu-item index="3">
-                    <template #title>
-                        设置
-                    </template>
-                </el-menu-item>
-            </el-menu>
-        </el-aside>
+        <!-- <el-container>
+        <el-header width="200px">
+            <el-button type="primary">人员维护</el-button>
+            <el-button type="primary">公积金维护</el-button>
+        </el-header>
         <el-main>Main</el-main>
-    </el-container>
+    </el-container> -->
+    <el-tabs type="card" style="height: 200px" class="main-tabs">
+        <el-tab-pane label="人员维护">
+            <el-button type="primary" @click="addAccount" style="float: left">增加人员</el-button>
+            <el-button type="primary" @click="addAccount" style="float: left">刷新</el-button>
+            
+            <el-table :data="userData" style="width: 100%">
+                <el-table-column type="index" ></el-table-column>
+                <el-table-column prop="name" label="姓名" ></el-table-column>
+                <el-table-column prop="id" label="身份证" ></el-table-column>
+                <el-table-column prop="dep" label="部门" ></el-table-column>
+                <el-table-column prop="bank" label="银行卡" ></el-table-column>
+                <el-table-column prop="percent" label="缴纳率" ></el-table-column>
+            </el-table>
+        </el-tab-pane>
+        <el-tab-pane label="公积金">
+
+        </el-tab-pane>
+        <el-tab-pane label="设置">
+
+        </el-tab-pane>
+    </el-tabs>
     </div>
 </template>
+<script setup>
+import {reactive} from 'vue'
+const userData =reactive([
+    {
+        name: "zhang3",
+        id: "110224198611111111",
+        dep: "政府",
+        bank: "1234567890",
+        percent: "5%",
+    },
+    {
+        name: "li4",
+        id: "110224198611111111",
+        dep: "政府",
+        bank: "1234567890",
+        percent: "12%",
+    },
+])
+</script>
+
+<style>
+.main-tabs > .el-tabs__content {
+  padding: 16px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+
+.el-tabs--right .el-tabs__content,
+.el-tabs--left .el-tabs__content {
+  height: 100%;
+}
+</style>
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
@@ -17,11 +18,11 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:            "pms",
-		Width:            1024,
-		Height:           768,
+		Fullscreen:       true,
 		Assets:           assets,
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 255, G: 255, B: 255, A: 1},
 		OnStartup:        app.startup,
+		Windows:          &windows.Options{Theme: windows.Light},
 		Bind: []interface{}{
 			app,
 		},
